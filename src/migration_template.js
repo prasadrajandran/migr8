dc15eb553fa4ea412bb0dd2352624823f94d2b96
Prasad Rajandran <prasadrajandran@gmail.com>
2020-08-03 13:13:14 -0400

    Add migration_template

---
+++ src/migration_template.js
@@ -0,0 +1,24 @@
+/**
+ * @file This migration does the following:
+ *   <TODO>
+ */
+
+/**
+ * Execute migration.
+ *
+ * @param {any} arg
+ * @returns {Promise<any>}
+ */
+exports.up = async (arg) => {
+  //
+};
+
+/**
+ * Rollback migration.
+ *
+ * @param {any} arg
+ * @returns {Promise<any>}
+ */
+exports.down = async (arg) => {
+  //
+};

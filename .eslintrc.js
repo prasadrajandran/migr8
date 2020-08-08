9bf0948097a45f0f445e78d05196855a33b4f2a9
Prasad Rajandran <prasadrajandran@gmail.com>
2020-08-08 00:08:36 -0400

    Add .eslintignore, .eslintrc.js, .gitignore, .prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+    es2017: true,
+  },
+  extends: ['standard', 'plugin:jsdoc/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 8,
+  },
+};

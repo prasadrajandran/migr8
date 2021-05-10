3ab9760f6712b23ca972a33d436d57f4fe3d4d7c
Prasad Rajandran <prasadrajandran@gmail.com>
2021-05-09 22:10:07 -0400

    Remove parseCommands and parseOptions

--- src/cli/helpers/parse_commands.ts
+++
@@ -1,9 +0,0 @@
-export const parseCommands = (
-  args: string[],
-  ...names: string[]
-): { name: string; value: string } => {
-  const pattern = new RegExp(names.join('|'));
-  const arg = args.find((a) => pattern.test(a)) || '';
-  const [name, value] = arg.split('=');
-  return { name, value };
-};

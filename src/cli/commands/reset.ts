73c29d9f497591f999ec7b6a9c5219dfaf21b6a7
Prasad Rajandran <prasadrajandran@gmail.com>
2021-05-09 22:00:32 -0400

    Remove command files (moving them into cli.js)

--- src/cli/commands/reset.ts
+++
@@ -1,9 +0,0 @@
-import { Migr8 } from '../../migr8';
-import { up } from './up';
-import { clear } from './clear';
-
-export const reset = async (migr8: Migr8): Promise<void> => {
-  if ((await clear(migr8)) === true) {
-    await up(migr8);
-  }
-};

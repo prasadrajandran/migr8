492e05e1f2c480a80c2bc2b63ad6d14f72061d0a
Prasad Rajandran <prasadrajandran@gmail.com>
2021-08-01 23:37:32 -0400

    Update wording

--- build.sh
+++ build.sh
@@ -35,9 +35,9 @@ package_version=${package_version/,/} # remove `,`
 echo
 echo "--- BUILD COMPLETE ---"
 echo
-echo "1. update the package version if necessary: $package_version"
-echo "2. do not forget to run \"npm install\" if the package version is updated so that the lockfile is updated too"
-echo "3. create PR to main (from development), merge PR, and create a GitHub release"
-echo "4. switch to the main branch locally and run \"git pull\""
+echo "1. update the package version (currently: \"$package_version\") if necessary"
+echo "2. run \"npm install\" if the package version is updated to update the version in the lockfile"
+echo "3. create PR to master (from development), merge PR, and create a GitHub release"
+echo "4. switch to the master branch locally and run \"git pull\""
 echo "5. to publish the package run: \"npm publish --access public\""
-echo "6. switch back to development, merge main, and run \"git push\""
+echo "6. switch back to development, merge master, and run \"git pull\""

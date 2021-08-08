99bef18e7cf3575880d322fde3544d091c3c309a
Prasad Rajandran <prasadrajandran@gmail.com>
2021-08-08 16:50:42 -0400

    Add sanitizeFilename tests

---
+++ tests/sanitize_filename.test.js
@@ -0,0 +1,30 @@
+const {
+  RESERVED_CHARACTERS,
+  RESERVED_WINDOWS_FILENAMES,
+  sanitizeFilename,
+} = require('../d/helpers/sanitize_filename');
+
+const REPLACEMENT = '_';
+
+const INVALID_ITEMS = RESERVED_CHARACTERS.concat(RESERVED_WINDOWS_FILENAMES)
+  .concat(RESERVED_WINDOWS_FILENAMES.map((name) => name.toLowerCase()))
+  .concat(RESERVED_WINDOWS_FILENAMES.map((name) => `${name}.txt`))
+  .concat('.');
+
+describe('sanitizeFilename()', () => {
+  test(`replaces an empty string with the replacement`, () => {
+    expect(sanitizeFilename('', REPLACEMENT)).toBe(REPLACEMENT);
+  });
+
+  INVALID_ITEMS.forEach((item) => {
+    test(`successfully sanitizes "${item}" to "${REPLACEMENT}"`, () => {
+      expect(sanitizeFilename(item, REPLACEMENT)).toBe(REPLACEMENT);
+    });
+  });
+
+  INVALID_ITEMS.forEach((item) => {
+    test(`not allowed to use "${item}" as a replacement`, () => {
+      expect(() => sanitizeFilename('_', item)).toThrow();
+    });
+  });
+});
